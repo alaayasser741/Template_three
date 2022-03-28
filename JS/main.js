c5de966056f91c19728e6fb9115e7f6b95e49f14
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-03-28 03:20:19 +0200

    Add onclick Event

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var img = document.getElementById("pre1");
+changePreview = () => img.src = "../img/video-preview.jpg";
+changePreview2 = () => img.src = "../img/video-preview2.png";
+changePreview3 = () => img.src = "../img/video-preview3.jpg";
+changePreview4 = () => img.src = "../img/video-preview4.jpg";
+changePreview5 = () => img.src = "../img/video-preview5.jpg";
+changePreview6 = () => img.src = "../img/video-preview6.png";
+changePreview7 = () => img.src = "../img/video-preview7.jpg";
+
